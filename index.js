96095132b58011cf41dabf37f7ec0b29db4e9e16
fugalfunkster <mtthwhsltn@gmail.com>
2016-07-26 01:06:41 -0700

    move from codepen to github repo

---
+++ index.js
@@ -0,0 +1,1189 @@
+var data = [
+  {
+    "coordinates": [
+      -113,
+      54
+    ],
+    "mass": 107000,
+    "name": "Abee"
+  },
+  {
+    "coordinates": [
+      0,
+      44
+    ],
+    "mass": 30000,
+    "name": "Agen"
+  },
+  {
+    "coordinates": [
+      42,
+      39
+    ],
+    "mass": 50000,
+    "name": "Akyumak"
+  },
+  {
+    "coordinates": [
+      10,
+      45
+    ],
+    "mass": 228000,
+    "name": "Alfianello"
+  },
+  {
+    "coordinates": [
+      -85,
+      42
+    ],
+    "mass": 32000,
+    "name": "Allegan"
+  },
+  {
+    "coordinates": [
+      -105,
+      26
+    ],
+    "mass": 2000000,
+    "name": "Allende"
+  },
+  {
+    "coordinates": [
+      3,
+      36
+    ],
+    "mass": 50000,
+    "name": "Aumale"
+  },
+  {
+    "coordinates": [
+      0,
+      43
+    ],
+    "mass": 50000,
+    "name": "Ausson"
+  },
+  {
+    "coordinates": [
+      -1,
+      52
+    ],
+    "mass": 44000,
+    "name": "Barwell"
+  },
+  {
+    "coordinates": [
+      -5,
+      15
+    ],
+    "mass": 29560,
+    "name": "Bassikounou"
+  },
+  {
+    "coordinates": [
+      -83,
+      38
+    ],
+    "mass": 86000,
+    "name": "Bath Furnace"
+  },
+  {
+    "coordinates": [
+      -8,
+      32
+    ],
+    "mass": 25000,
+    "name": "Benguerir"
+  },
+  {
+    "coordinates": [
+      -7,
+      30
+    ],
+    "mass": 45000,
+    "name": "Bensour"
+  },
+  {
+    "coordinates": [
+      0,
+      12
+    ],
+    "mass": 25000,
+    "name": "Bilanga"
+  },
+  {
+    "coordinates": [
+      25,
+      60
+    ],
+    "mass": 330000,
+    "name": "Bjurböle"
+  },
+  {
+    "coordinates": [
+      131,
+      44
+    ],
+    "mass": 256000,
+    "name": "Boguslavka"
+  },
+  {
+    "coordinates": [
+      16,
+      50
+    ],
+    "mass": 39000,
+    "name": "Braunau"
+  },
+  {
+    "coordinates": [
+      -112,
+      53
+    ],
+    "mass": 303000,
+    "name": "Bruderheim"
+  },
+  {
+    "coordinates": [
+      48,
+      5
+    ],
+    "mass": 120000,
+    "name": "Bur-Gheluai"
+  },
+  {
+    "coordinates": [
+      29,
+      40
+    ],
+    "mass": 25000,
+    "name": "Bursa"
+  },
+  {
+    "coordinates": [
+      84,
+      27
+    ],
+    "mass": 29000,
+    "name": "Butsura"
+  },
+  {
+    "coordinates": [
+      -109,
+      52
+    ],
+    "mass": 41000,
+    "name": "Buzzard Coulee"
+  },
+  {
+    "coordinates": [
+      -1,
+      37
+    ],
+    "mass": 25000,
+    "name": "Cabezo de Mayo"
+  },
+  {
+    "coordinates": [
+      -93,
+      35
+    ],
+    "mass": 48500,
+    "name": "Cabin Creek"
+  },
+  {
+    "coordinates": [
+      -5,
+      43
+    ],
+    "mass": 34000,
+    "name": "Cangas de Onis"
+  },
+  {
+    "coordinates": [
+      1,
+      46
+    ],
+    "mass": 31500,
+    "name": "Chantonnay"
+  },
+  {
+    "coordinates": [
+      1,
+      47
+    ],
+    "mass": 27000,
+    "name": "Charsonville"
+  },
+  {
+    "coordinates": [
+      2,
+      47
+    ],
+    "mass": 30000,
+    "name": "Château-Renard"
+  },
+  {
+    "coordinates": [
+      61,
+      54
+    ],
+    "mass": 100000,
+    "name": "Chelyabinsk"
+  },
+  {
+    "coordinates": [
+      -5,
+      23
+    ],
+    "mass": 100000,
+    "name": "Chergach "
+  },
+  {
+    "coordinates": [
+      -90,
+      44
+    ],
+    "mass": 104000,
+    "name": "Colby (Wisconsin)"
+  },
+  {
+    "coordinates": [
+      73,
+      25
+    ],
+    "mass": 25400,
+    "name": "Desuri"
+  },
+  {
+    "coordinates": [
+      71,
+      22
+    ],
+    "mass": 45000,
+    "name": "Dhajala"
+  },
+  {
+    "coordinates": [
+      76,
+      32
+    ],
+    "mass": 32000,
+    "name": "Dhurmsala"
+  },
+  {
+    "coordinates": [
+      111,
+      -7
+    ],
+    "mass": 166000,
+    "name": "Djati-Pengilon"
+  },
+  {
+    "coordinates": [
+      119,
+      45
+    ],
+    "mass": 128800,
+    "name": "Dong Ujimqin Qi"
+  },
+  {
+    "coordinates": [
+      -82,
+      42
+    ],
+    "mass": 47700,
+    "name": "Dresden (Ontario)"
+  },
+  {
+    "coordinates": [
+      12,
+      50
+    ],
+    "mass": 107000,
+    "name": "Elbogen"
+  },
+  {
+    "coordinates": [
+      37,
+      47
+    ],
+    "mass": 54640,
+    "name": "Elenovka"
+  },
+  {
+    "coordinates": [
+      7,
+      47
+    ],
+    "mass": 127000,
+    "name": "Ensisheim"
+  },
+  {
+    "coordinates": [
+      -94,
+      43
+    ],
+    "mass": 320000,
+    "name": "Estherville"
+  },
+  {
+    "coordinates": [
+      -97,
+      39
+    ],
+    "mass": 89400,
+    "name": "Farmington"
+  },
+  {
+    "coordinates": [
+      -77,
+      35
+    ],
+    "mass": 56000,
+    "name": "Farmville"
+  },
+  {
+    "coordinates": [
+      -93,
+      43
+    ],
+    "mass": 152000,
+    "name": "Forest City"
+  },
+  {
+    "coordinates": [
+      146,
+      -33
+    ],
+    "mass": 26000,
+    "name": "Forest Vale"
+  },
+  {
+    "coordinates": [
+      37,
+      57
+    ],
+    "mass": 152000,
+    "name": "Glasatovo"
+  },
+  {
+    "coordinates": [
+      -6,
+      38
+    ],
+    "mass": 39000,
+    "name": "Guareña"
+  },
+  {
+    "coordinates": [
+      11,
+      11
+    ],
+    "mass": 100000,
+    "name": "Gujba"
+  },
+  {
+    "coordinates": [
+      42,
+      9
+    ],
+    "mass": 34650,
+    "name": "Gursum"
+  },
+  {
+    "coordinates": [
+      -1,
+      51
+    ],
+    "mass": 29000,
+    "name": "Hatford"
+  },
+  {
+    "coordinates": [
+      -110,
+      34
+    ],
+    "mass": 220000,
+    "name": "Holbrook"
+  },
+  {
+    "coordinates": [
+      -91,
+      41
+    ],
+    "mass": 230000,
+    "name": "Homestead"
+  },
+  {
+    "coordinates": [
+      16,
+      46
+    ],
+    "mass": 49000,
+    "name": "Hraschina"
+  },
+  {
+    "coordinates": [
+      46,
+      39
+    ],
+    "mass": 27000,
+    "name": "Indarch"
+  },
+  {
+    "coordinates": [
+      21,
+      -32
+    ],
+    "mass": 48000,
+    "name": "Jackalsfontein"
+  },
+  {
+    "coordinates": [
+      20,
+      43
+    ],
+    "mass": 34000,
+    "name": "Jelica"
+  },
+  {
+    "coordinates": [
+      109,
+      30
+    ],
+    "mass": 600000,
+    "name": "Jianshi"
+  },
+  {
+    "coordinates": [
+      126,
+      44
+    ],
+    "mass": 4000000,
+    "name": "Jilin"
+  },
+  {
+    "coordinates": [
+      -104,
+      40
+    ],
+    "mass": 40300,
+    "name": "Johnstown"
+  },
+  {
+    "coordinates": [
+      115,
+      35
+    ],
+    "mass": 100000,
+    "name": "Juancheng"
+  },
+  {
+    "coordinates": [
+      111,
+      -7
+    ],
+    "mass": 32490,
+    "name": "Jumapalo"
+  },
+  {
+    "coordinates": [
+      -7,
+      38
+    ],
+    "mass": 25250,
+    "name": "Juromenha"
+  },
+  {
+    "coordinates": [
+      4,
+      44
+    ],
+    "mass": 91000,
+    "name": "Juvinas"
+  },
+  {
+    "coordinates": [
+      53,
+      55
+    ],
+    "mass": 200000,
+    "name": "Kainsaz"
+  },
+  {
+    "coordinates": [
+      139,
+      -35
+    ],
+    "mass": 41730,
+    "name": "Karoonda"
+  },
+  {
+    "coordinates": [
+      31,
+      39
+    ],
+    "mass": 85000,
+    "name": "Kayakent"
+  },
+  {
+    "coordinates": [
+      -3,
+      48
+    ],
+    "mass": 80000,
+    "name": "Kernouve"
+  },
+  {
+    "coordinates": [
+      141,
+      38
+    ],
+    "mass": 135000,
+    "name": "Kesen"
+  },
+  {
+    "coordinates": [
+      28,
+      14
+    ],
+    "mass": 100000,
+    "name": "Kidairat"
+  },
+  {
+    "coordinates": [
+      22,
+      48
+    ],
+    "mass": 500000,
+    "name": "Knyahinya"
+  },
+  {
+    "coordinates": [
+      30,
+      47
+    ],
+    "mass": 50000,
+    "name": "Krymka"
+  },
+  {
+    "coordinates": [
+      61,
+      55
+    ],
+    "mass": 200000,
+    "name": "Kunashak"
+  },
+  {
+    "coordinates": [
+      59,
+      42
+    ],
+    "mass": 1100000,
+    "name": "Kunya-Urgench"
+  },
+  {
+    "coordinates": [
+      76,
+      10
+    ],
+    "mass": 45000,
+    "name": "Kuttippuram"
+  },
+  {
+    "coordinates": [
+      130,
+      32
+    ],
+    "mass": 45000,
+    "name": "Kyushu"
+  },
+  {
+    "coordinates": [
+      -58,
+      -31
+    ],
+    "mass": 45000,
+    "name": "La Criolla"
+  },
+  {
+    "coordinates": [
+      0,
+      48
+    ],
+    "mass": 37000,
+    "name": "L'Aigle"
+  },
+  {
+    "coordinates": [
+      1,
+      47
+    ],
+    "mass": 51700,
+    "name": "Lancé"
+  },
+  {
+    "coordinates": [
+      -99,
+      35
+    ],
+    "mass": 51500,
+    "name": "Leedey"
+  },
+  {
+    "coordinates": [
+      -8,
+      52
+    ],
+    "mass": 50000,
+    "name": "Limerick"
+  },
+  {
+    "coordinates": [
+      72,
+      26
+    ],
+    "mass": 40000,
+    "name": "Lohawat"
+  },
+  {
+    "coordinates": [
+      19,
+      52
+    ],
+    "mass": 59000,
+    "name": "Łowicz"
+  },
+  {
+    "coordinates": [
+      35,
+      -15
+    ],
+    "mass": 93200,
+    "name": "Machinga"
+  },
+  {
+    "coordinates": [
+      95,
+      27
+    ],
+    "mass": 70500,
+    "name": "Mahadevpur"
+  },
+  {
+    "coordinates": [
+      -91,
+      41
+    ],
+    "mass": 28400,
+    "name": "Marion (Iowa)"
+  },
+  {
+    "coordinates": [
+      30,
+      61
+    ],
+    "mass": 45000,
+    "name": "Marjalahti"
+  },
+  {
+    "coordinates": [
+      34,
+      1
+    ],
+    "mass": 150000,
+    "name": "Mbale"
+  },
+  {
+    "coordinates": [
+      81,
+      25
+    ],
+    "mass": 71400,
+    "name": "Merua"
+  },
+  {
+    "coordinates": [
+      120,
+      -26
+    ],
+    "mass": 330000,
+    "name": "Millbillillie"
+  },
+  {
+    "coordinates": [
+      24,
+      46
+    ],
+    "mass": 300000,
+    "name": "Mocs"
+  },
+  {
+    "coordinates": [
+      -101,
+      38
+    ],
+    "mass": 35000,
+    "name": "Modoc (1905)"
+  },
+  {
+    "coordinates": [
+      -1,
+      38
+    ],
+    "mass": 144000,
+    "name": "Molina"
+  },
+  {
+    "coordinates": [
+      1,
+      43
+    ],
+    "mass": 149000,
+    "name": "Montferré"
+  },
+  {
+    "coordinates": [
+      4,
+      18
+    ],
+    "mass": 110000,
+    "name": "Mount Tazerzait"
+  },
+  {
+    "coordinates": [
+      145,
+      -36
+    ],
+    "mass": 100000,
+    "name": "Murchison"
+  },
+  {
+    "coordinates": [
+      103,
+      35
+    ],
+    "mass": 52900,
+    "name": "Nan Yang Pao"
+  },
+  {
+    "coordinates": [
+      -81,
+      40
+    ],
+    "mass": 230000,
+    "name": "New Concord"
+  },
+  {
+    "coordinates": [
+      -4,
+      13
+    ],
+    "mass": 37500,
+    "name": "N'Goureyma"
+  },
+  {
+    "coordinates": [
+      -99,
+      39
+    ],
+    "mass": 1100000,
+    "name": "Norton County"
+  },
+  {
+    "coordinates": [
+      -102,
+      24
+    ],
+    "mass": 50000,
+    "name": "Nuevo Mercurio"
+  },
+  {
+    "coordinates": [
+      55,
+      57
+    ],
+    "mass": 500000,
+    "name": "Ochansk"
+  },
+  {
+    "coordinates": [
+      -7,
+      38
+    ],
+    "mass": 150000,
+    "name": "Olivenza"
+  },
+  {
+    "coordinates": [
+      -2,
+      39
+    ],
+    "mass": 40000,
+    "name": "Olmedilla de Alarcón"
+  },
+  {
+    "coordinates": [
+      161,
+      64
+    ],
+    "mass": 250000,
+    "name": "Omolon"
+  },
+  {
+    "coordinates": [
+      -90,
+      36
+    ],
+    "mass": 408000,
+    "name": "Paragould"
+  },
+  {
+    "coordinates": [
+      -51,
+      -19
+    ],
+    "mass": 100000,
+    "name": "Paranaiba"
+  },
+  {
+    "coordinates": [
+      78,
+      9
+    ],
+    "mass": 77600,
+    "name": "Parnallee"
+  },
+  {
+    "coordinates": [
+      91,
+      23
+    ],
+    "mass": 37350,
+    "name": "Patwar"
+  },
+  {
+    "coordinates": [
+      35,
+      48
+    ],
+    "mass": 40000,
+    "name": "Pavlograd"
+  },
+  {
+    "coordinates": [
+      -117,
+      56
+    ],
+    "mass": 45760,
+    "name": "Peace River"
+  },
+  {
+    "coordinates": [
+      -103,
+      30
+    ],
+    "mass": 70000,
+    "name": "Peña Blanca Spring"
+  },
+  {
+    "coordinates": [
+      39,
+      56
+    ],
+    "mass": 66000,
+    "name": "Pervomaisky"
+  },
+  {
+    "coordinates": [
+      -99,
+      40
+    ],
+    "mass": 57900,
+    "name": "Phillips County (stone)"
+  },
+  {
+    "coordinates": [
+      73,
+      26
+    ],
+    "mass": 42000,
+    "name": "Piplia Kalan"
+  },
+  {
+    "coordinates": [
+      -103,
+      34
+    ],
+    "mass": 71400,
+    "name": "Portales Valley"
+  },
+  {
+    "coordinates": [
+      21,
+      52
+    ],
+    "mass": 250000,
+    "name": "Pultusk"
+  },
+  {
+    "coordinates": [
+      -53,
+      -29
+    ],
+    "mass": 300000,
+    "name": "Putinga"
+  },
+  {
+    "coordinates": [
+      70,
+      28
+    ],
+    "mass": 67225,
+    "name": "Rahimyar Khan"
+  },
+  {
+    "coordinates": [
+      -102,
+      46
+    ],
+    "mass": 90000,
+    "name": "Richardton"
+  },
+  {
+    "coordinates": [
+      0,
+      45
+    ],
+    "mass": 271000,
+    "name": "Saint-Séverin"
+  },
+  {
+    "coordinates": [
+      46,
+      52
+    ],
+    "mass": 200000,
+    "name": "Saratov"
+  },
+  {
+    "coordinates": [
+      36,
+      50
+    ],
+    "mass": 101000,
+    "name": "Sevrukovo"
+  },
+  {
+    "coordinates": [
+      134,
+      46
+    ],
+    "mass": 23000000,
+    "name": "Sikhote-Alin"
+  },
+  {
+    "coordinates": [
+      36,
+      39
+    ],
+    "mass": 40000,
+    "name": "Sivas"
+  },
+  {
+    "coordinates": [
+      21,
+      43
+    ],
+    "mass": 80000,
+    "name": "Soko-Banja"
+  },
+  {
+    "coordinates": [
+      125,
+      45
+    ],
+    "mass": 36900,
+    "name": "Songyuan"
+  },
+  {
+    "coordinates": [
+      14,
+      59
+    ],
+    "mass": 34000,
+    "name": "Ställdalen"
+  },
+  {
+    "coordinates": [
+      15,
+      49
+    ],
+    "mass": 52000,
+    "name": "Stannern"
+  },
+  {
+    "coordinates": [
+      55,
+      53
+    ],
+    "mass": 325000,
+    "name": "Sterlitamak"
+  },
+  {
+    "coordinates": [
+      -72,
+      45
+    ],
+    "mass": 25400,
+    "name": "St-Robert"
+  },
+  {
+    "coordinates": [
+      113,
+      31
+    ],
+    "mass": 260000,
+    "name": "Suizhou"
+  },
+  {
+    "coordinates": [
+      78,
+      12
+    ],
+    "mass": 110000,
+    "name": "Sulagiri"
+  },
+  {
+    "coordinates": [
+      -7,
+      31
+    ],
+    "mass": 100000,
+    "name": "Tamdakht"
+  },
+  {
+    "coordinates": [
+      142,
+      -25
+    ],
+    "mass": 160000,
+    "name": "Tenham"
+  },
+  {
+    "coordinates": [
+      26,
+      58
+    ],
+    "mass": 28500,
+    "name": "Tennasilm"
+  },
+  {
+    "coordinates": [
+      27,
+      -29
+    ],
+    "mass": 45300,
+    "name": "Thuathe"
+  },
+  {
+    "coordinates": [
+      17,
+      49
+    ],
+    "mass": 28000,
+    "name": "Tieschitz"
+  },
+  {
+    "coordinates": [
+      -89,
+      38
+    ],
+    "mass": 74800,
+    "name": "Tilden"
+  },
+  {
+    "coordinates": [
+      35,
+      54
+    ],
+    "mass": 65500,
+    "name": "Timochin"
+  }
+];
+
+data.map(function(each){
+  each.mass < 50000 ? each.rad = 1 : each.mass < 100000 ? each.rad = 2 : each.mass < 250000 ? each.rad = 3 : each.mass < 500000 ? each.rad = 5 : each.mass < 1000000 ? each.rad = 7 : each.mass < 1500000 ? each.rad = 9 : each.mass < 2000000 ? each.rad = 12 : each.mass < 3000000 ? each.rad = 18 : each.mass < 4000000 ? each.rad = 24 : each.mass < 5000000 ? each.rad = 30 : each.rad = 36 ;
+})
+
+var width = 1000,
+    height = 1000;
+
+var projection = d3.geo.peirceQuincuncial()
+    .rotate([-70, -90, 45])
+    .scale(215)
+    .translate([width / 2, height / 2])
+    .precision(.1);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var graticule = d3.geo.graticule();
+
+var svg = d3.select("div").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("path")
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
+
+
+ var tooltip = d3.select("div").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+ svg.selectAll("circle")
+		.data(data).enter()
+		.append("circle")
+		.attr("cx", function (d) { return projection(d.coordinates)[0]; })
+		.attr("cy", function (d) { return projection(d.coordinates)[1]; })
+		.attr("r", function(d){ return parseInt(d.rad)})
+    .attr("d", path)
+		.attr("fill", "red")
+ .on("mouseover", function(d){
+  d3.select(this).style("fill", "orange");
+  tooltip.style("opacity", .95);
+  tooltip.html("<div><p>" + d.name + "</p><p> Mass: " + d.mass + " g</p></div>")
+    .style("left", (d3.event.pageX + 18) + "px")
+    .style("top", (d3.event.pageY - 28) + "px");
+  
+  })
+.on("mouseout", function(){
+  
+    d3.select(this).style("fill", "red");
+  
+    tooltip.style("opacity", 0);
+  
+  });;
+
+
+
+d3.json("https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-50m.json", function(error, world) {
+  if (error) throw error;
+
+  
+  svg.insert("path", ".graticule")
+      .datum(topojson.feature(world, world.objects.land))
+      .attr("class", "land")
+      .attr("d", path);
+
+  svg.insert("path", ".graticule")
+      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+      .attr("class", "boundary")
+      .attr("d", path);
+});
+
+d3.select(self.frameElement).style("height", height + "px");
